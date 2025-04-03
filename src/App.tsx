900bfc4c64c4fb68ec01c3524c57473796eb1c93
Thapa-Dipesh <dthapa4682@gmail.com>
2025-04-03 08:38:37 +0545

    brands section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Layout } from "./components/Layout";
+import { Brands } from "./components/sections/Brands";
 import { Hero } from "./components/sections/Hero";
 
 function App() {
-  return <Layout title="EdgeAI">
-    <Hero />
-  </Layout>;
+  return (
+    <Layout title="EdgeAI">
+      <Hero />
+      <Brands />
+    </Layout>
+  );
 }
 
 export default App;
